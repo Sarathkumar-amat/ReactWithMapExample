34b6a03e9d385f29328e8457061f4e18d09c7b57
Sarathkumar-amat <69066317+Sarathkumar-amat@users.noreply.github.com>
2023-03-14 08:30:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+const items = [
+  { id: 1, name: "Book", price: 10 },
+  { id: 2, name: "T-Shirt", price: 15 },
+  { id: 3, name: "Hat", price: 8 },
+  { id: 4, name: "Sunglasses", price: 12 }
+];
+
+const total = 45;
+export default function App() {
+  return (
+    <div>
+      <h1>Shopping Cart</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name}: ({item.price})
+          </li>
+        ))}
+      </ul>
+      <p>Total: {total}</p>
+    </div>
+  );
+}
